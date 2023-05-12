c37cbb5ae7fc64974d5b0f308c69fadd979ea092
William Medrado <30448574+Wmedrado@users.noreply.github.com>
2023-05-12 11:01:51 -0300

    Add files via upload

---
+++ desafio_raio.js
@@ -0,0 +1,10 @@
+let a = 3;
+const PI = 3.14159265359
+let raio = 10;
+Resultado = PI * raio * raio
+
+Math.PI // PI do JavaScript 
+
+console.log(Resultado);
+console.log(typeof Resultado);
+console.log("O resultado é " + Math.PI * raio * raio);
