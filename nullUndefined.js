4fa2ec54495b0c35b803b9bf60a64270180c637f
William Medrado <30448574+Wmedrado@users.noreply.github.com>
2023-05-20 00:46:48 -0300

    Add files via upload

---
+++ nullUndefined.js
@@ -0,0 +1,10 @@
+let preco = null
+// console.log(preco.nome) // Vai dar erro! pois a variavel preco precisa possuir um valor nao nulo.
+preco = 10
+console.log(preco.nome)
+
+let prod1 = 'Lapis'
+console.log(prod1.preco = null) //sem preço
+
+let prod2 = {}
+console.log(prod1.nome)
---
+++ objeto.js
@@ -0,0 +1,20 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+prod1 ['Desconto Legal'] = 0.40 // Evitar atributos com espaços
+
+console.log(prod1)
+const prod2 = {
+    nome: 'Camisa Polo', 
+    preco: 79.90
+}
+"{nome: 'Camisa Polo', preco  79.90}"
+
+console.log(prod2)
+
+prod3 = {
+    Nome: 'Notebook',
+    Preco: 3980,
+    Configuracao: 'Ryzen 5 com 12Gb de RAM, placa de vídeo GTX 960'
+ }
+ console.log(prod3)
